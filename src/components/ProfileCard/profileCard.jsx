bb8e00d9c2e84cfcb7eb2886cf68ef2e4dce5cce
Andrew9806-lang <kornilovaamina20@gmail.com>
2025-10-31 20:19:13 +0300

    Change_Homework

--- src/components/ProfileCard/profileCard.jsx
+++ src/components/ProfileCard/profileCard.jsx
@@ -2,20 +2,21 @@ import "./styles.css";
 import { info } from "./data";
 import Button from "../Button/Button";
 
-function ProfileCard(){
-    const getInfo = ()=>{
-        return `${info.name} ${info.surname}`
-    };
+function ProfileCard() {
+    const  {imagsrc,name,surname,proffession,hobby}=info;
+    // distuktizaciya objekta dlya udobstva 
+  const getInfo = () => {
+    return `${info.name} ${info.surname}`;
+  };
 
-    return(
-        <div className="profileCard-wrapper">
-            <img src={info.imgSrc}/>
-            <h3>{getInfo()}</h3>
-            <p>Professional:{info.proffession}</p>
-            <p className="hobby">hobby:{info.hobby}</p>
-            <Button/>
-        </div>
-    );
+  return (
+    <div className="profileCard-wrapper">
+      <img src={info.imgSrc} />
+      <h3>{getInfo()}</h3>
+      <p>Professional:{info.proffession}</p>
+      <p className="hobby">hobby:{info.hobby}</p>
+    </div>
+  );
 }
 
 export default ProfileCard;
