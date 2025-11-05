4c13c8ca54ff09a28757d10bcc48faf2ff555c12
Andrew9806-lang <kornilovaamina20@gmail.com>
2025-11-05 12:13:42 +0300

    Lesson05

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,24 @@
 import "./styles.css";
 
-// Компонент создаётся с помощью слова function и обязательно!
-// с большой буквы. Компонент обязательно должен возвращать JSX
-// иначе он просто не будет компонентом
+// sozdaetsya s pomoshyu slovo function 
+// ! obyazatelno s pomoshyu bolshoi bukvi 
+// komponent dolzhen obyazaterlno dolzhen vozvrashat jsx incache on prosto ne budet komponentom
+
 //u funkcionalnih komponentov est parametr nazivaestya props
 // etot parametr pozvolyaet v moment vizova peredat v komponent znacheniya 
 // to est mi zadaem object i pishem tuda znachenie kotorie hotim 
 function Button({type = "button",onClick,name}){
     return <button className="button" type={type} onClick={onClick}>{name}</button>;
+}
+//export komponenta po umolchaniyu komponenta button s pomoshyu eksporta
+// mi mozhem poluchat komponent v drugih komponentah i tam ego ispolzovat 
+// inache komponent budet nedostupen v drugih failah projejta 
 
-// Экспорт по умолчанию компонента Button. С помощью экспорта
-// мы можем получать компонент в других компонентах и там его
-// использовать, то есть вызывать. Иначе компонент будет недоступен
-// в других файлах проекта
 export default Button;
 
-// const props = {
-//  name:
-//  type:
-// }
+
+// const props eto object = {
+// name:
+// type:
+
+//}
